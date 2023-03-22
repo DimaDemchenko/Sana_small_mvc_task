266b402b6e96895e16e652172f3cf2cb21062bce
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2023-03-22 17:52:01 +0200

    added User model

---
+++ SMALL_TASK_MVC/Models/User.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SMALL_TASK_MVC.Models
+{
+    public class User
+    {
+        [Display(Name = "Enter first name:")]
+        [Required(ErrorMessage = "Has to be filled!")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Enter surname:")]
+        [Required(ErrorMessage = "Has to be filled!")]
+        public string Surname { get; set; }
+    }
+}
